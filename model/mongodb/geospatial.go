744ff4611d8a6adf32399702f85218761134bcef
truanv <truanv@vnpt.vn>
2021-03-22 15:13:49 +0700

    add min and max distance

--- model/mongodb/geospatial.go
+++ model/mongodb/geospatial.go
@@ -6,6 +6,8 @@ type (
 		Type         string     `json:"type" validate:"required"`
 		CoorName     string     `json:"coor_name" validate:"required"`
 		Coor         Coordinate `json:"coordinate" validate:"required"`
+		MaxDistance  float64    `json:"max_distance" validate:"required"`
+		MinDistance  float64    `json:"min_distance" validate:"required"`
 	}
 	Coordinate struct {
 		Long float64 `json:"long" validate:"required"`
